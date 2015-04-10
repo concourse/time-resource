1efe9904ce0404fe8779f6673d99d448278c34ee
Chris Brown <cbrown@pivotal.io>
2015-04-10 09:35:26 -0700

    add test for invalid durations
    Signed-off-by: Toby Rumans <trumans@pivotal.io>

--- check/check_test.go
+++ check/check_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/concourse/time-resource/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
@@ -18,15 +19,47 @@ var _ = Describe("Check", func() {
 		checkCmd = exec.Command(checkPath)
 	})
 
+	Context("with invalid inputs", func() {
+		var request models.CheckRequest
+		var response models.CheckResponse
+		var session *gexec.Session
+
+		BeforeEach(func() {
+			request = models.CheckRequest{}
+			response = models.CheckResponse{}
+		})
+
+		JustBeforeEach(func() {
+			var err error
+
+			stdin, err := checkCmd.StdinPipe()
+			Ω(err).ShouldNot(HaveOccurred())
+
+			session, err = gexec.Start(checkCmd, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			err = json.NewEncoder(stdin).Encode(request)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		Context("with an invalid interval ", func() {
+			BeforeEach(func() {
+				request.Source.Interval = "not-an-interval"
+			})
+
+			It("returns an error", func() {
+				Eventually(session.Err).Should(gbytes.Say("invalid interval"))
+				Eventually(session).Should(gexec.Exit(1))
+			})
+		})
+	})
+
 	Context("when executed", func() {
 		var request models.CheckRequest
 		var response models.CheckResponse
 
 		BeforeEach(func() {
-			request = models.CheckRequest{
-				Source: models.Source{Interval: "1m"},
-			}
-
+			request = models.CheckRequest{}
 			response = models.CheckResponse{}
 		})
 
@@ -46,43 +79,49 @@ var _ = Describe("Check", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 		})
 
-		Context("when no version is given", func() {
-			It("outputs a version containing the current time", func() {
-				Ω(response).Should(HaveLen(1))
-				Ω(response[0].Time.Unix()).Should(BeNumerically("~", time.Now().Unix(), 1))
-			})
-		})
-
-		Context("when a version is given", func() {
-			Context("with its time within the interval", func() {
-				BeforeEach(func() {
-					request.Version.Time = time.Now()
-				})
-
-				It("does not output any versions", func() {
-					Ω(response).Should(BeEmpty())
-				})
+		Context("when an interval is specified", func() {
+			BeforeEach(func() {
+				request.Source.Interval = "1m"
 			})
 
-			Context("with its time one interval ago", func() {
-				BeforeEach(func() {
-					request.Version.Time = time.Now().Add(-1 * time.Minute)
-				})
-
+			Context("when no version is given", func() {
 				It("outputs a version containing the current time", func() {
 					Ω(response).Should(HaveLen(1))
 					Ω(response[0].Time.Unix()).Should(BeNumerically("~", time.Now().Unix(), 1))
 				})
 			})
 
-			Context("with its time N intervals ago", func() {
-				BeforeEach(func() {
-					request.Version.Time = time.Now().Add(-5 * time.Minute)
+			Context("when a version is given", func() {
+				Context("with its time within the interval", func() {
+					BeforeEach(func() {
+						request.Version.Time = time.Now()
+					})
+
+					It("does not output any versions", func() {
+						Ω(response).Should(BeEmpty())
+					})
 				})
 
-				It("outputs a version containing the current time", func() {
-					Ω(response).Should(HaveLen(1))
-					Ω(response[0].Time.Unix()).Should(BeNumerically("~", time.Now().Unix(), 1))
+				Context("with its time one interval ago", func() {
+					BeforeEach(func() {
+						request.Version.Time = time.Now().Add(-1 * time.Minute)
+					})
+
+					It("outputs a version containing the current time", func() {
+						Ω(response).Should(HaveLen(1))
+						Ω(response[0].Time.Unix()).Should(BeNumerically("~", time.Now().Unix(), 1))
+					})
+				})
+
+				Context("with its time N intervals ago", func() {
+					BeforeEach(func() {
+						request.Version.Time = time.Now().Add(-5 * time.Minute)
+					})
+
+					It("outputs a version containing the current time", func() {
+						Ω(response).Should(HaveLen(1))
+						Ω(response[0].Time.Unix()).Should(BeNumerically("~", time.Now().Unix(), 1))
+					})
 				})
 			})
 		})
--- check/main.go
+++ check/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"time"
 
@@ -15,19 +16,19 @@ func main() {
 
 	err := json.NewDecoder(os.Stdin).Decode(&request)
 	if err != nil {
-		println("error decoding payload: " + err.Error())
+		fmt.Fprintln(os.Stderr, "error decoding payload: "+err.Error())
 		os.Exit(1)
 	}
 
-	duration, err := time.ParseDuration(request.Source.Interval)
+	interval, err := time.ParseDuration(request.Source.Interval)
 	if err != nil {
-		println("invalid interval: " + request.Source.Interval + "; " + err.Error())
+		fmt.Fprintln(os.Stderr, "invalid interval: "+request.Source.Interval+"; "+err.Error())
 		os.Exit(1)
 	}
 
 	versions := []models.Version{}
 
-	if now.Sub(request.Version.Time) > duration {
+	if now.Sub(request.Version.Time) > interval {
 		versions = append(versions, models.Version{Time: now})
 	}
 
