2f5e4f21f303ee1413e13d32334b9e06d582b272
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-04-04 04:04:55 +0900

    add version samles to test
    Signed-off-by: cappyzawa <cappyzawa@yahoo.ne.jp>

--- out/out_test.go
+++ out/out_test.go
@@ -80,12 +80,16 @@ var _ = Describe("Out", func() {
 			})
 
 			It("reports specified location's current time(offset: -0400) as the version", func() {
+				// An example of response.Version.Time.String() is
+				// 2019-04-03 14:53:10.951241 -0400 EDT
 				contained := strings.Contains(response.Version.Time.String(), "-0400")
 				Expect(contained).To(BeTrue())
 			})
 		})
 		Context("when a location is not specified", func() {
 			It("reports the current time(offset: 0000) as the version", func() {
+				// An example of response.Version.Time.String() is
+				// 2019-04-03 18:53:10.964705 +0000 UTC
 				contained := strings.Contains(response.Version.Time.String(), "0000")
 				Expect(contained).To(BeTrue())
 			})
